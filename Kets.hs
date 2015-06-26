778d8f966b08747876a98204ed1ca3f832718ecf
Reid Wagner <rwagner@Reids-MacBook-Pro.local>
2015-06-26 18:26:47 -0500

    a few comments

--- Kets.hs
+++ Kets.hs
@@ -6,6 +6,7 @@ import Data.Complex
 norm :: [[Complex Float]] -> [[Complex Float]]
 norm vect = let sq2 = 1/(sqrt 2.0) :: Complex Float in [map (*sq2) xs | xs <- vect]
 
+--Kets
 kz0 = [[1 :+ 0],[0 :+ 0]] :: [[Complex Float]]
 kz1 = [[0],[1]] :: [[Complex Float]]
 kx0 = [[1],[1]] :: [[Complex Float]]
@@ -16,8 +17,10 @@ ky1 = [[1],[0 :+ 1]] :: [[Complex Float]]
 operate :: [[Complex Float]] -> [[Complex Float]] -> [[Complex Float]]
 operate [[x],[y]] [[a,b],[c,d]] =  [[(x*a)+(y*b)], [(x*c)+(y*d)]]
 
+--Gate data type
 data Gate = Sz | Sx | Sy | Had | L
 
+--'g'ate 'Op'eration
 gOp :: Gate -> [[Complex Float]] -> [[Complex Float]]
 gOp Sz xs = operate xs [[1,0],[0,-1]]
 gOp Sx xs = operate xs [[0,1],[1,0]]
