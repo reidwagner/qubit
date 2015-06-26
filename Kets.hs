2dca9d7571f0f81ba35ad1b31bcaa35cf163318f
Reid Wagner <rwagner@Reids-MacBook-Pro.local>
2015-06-26 18:23:03 -0500

    second commit

--- Kets.hs
+++ Kets.hs
@@ -1,9 +1,10 @@
 module Kets
-( kz0, kz1, kx0, kx1, ky0, ky1, gOp) where
+( kz0, kz1, kx0, kx1, ky0, ky1, gOp, Gate) where
 
 import Data.Complex
 
-c = 1/(sqrt 2)
+norm :: [[Complex Float]] -> [[Complex Float]]
+norm vect = let sq2 = 1/(sqrt 2.0) :: Complex Float in [map (*sq2) xs | xs <- vect]
 
 kz0 = [[1 :+ 0],[0 :+ 0]] :: [[Complex Float]]
 kz1 = [[0],[1]] :: [[Complex Float]]
@@ -15,9 +16,11 @@ ky1 = [[1],[0 :+ 1]] :: [[Complex Float]]
 operate :: [[Complex Float]] -> [[Complex Float]] -> [[Complex Float]]
 operate [[x],[y]] [[a,b],[c,d]] =  [[(x*a)+(y*b)], [(x*c)+(y*d)]]
 
-gOp :: [[Complex Float]] -> String -> [[Complex Float]]
-gOp xs "sgz" = operate xs [[1,0],[0,-1]]
-gOp xs "sgx" = operate xs [[1,0],[0,-1]]
-gOp xs "sgy" = operate xs [[1,0],[0,-1]]
-gOp xs "had" = operate xs [[1,0],[0,-1]]
-gOp xs "l" = operate xs [[1,0],[0,-1]]  
+data Gate = Sz | Sx | Sy | Had | L
+
+gOp :: Gate -> [[Complex Float]] -> [[Complex Float]]
+gOp Sz xs = operate xs [[1,0],[0,-1]]
+gOp Sx xs = operate xs [[0,1],[1,0]]
+gOp Sy xs = operate xs [[0,0 :+ (-1)],[0 :+ 1,0]]
+gOp Had xs = norm $ operate xs [[1,1],[1,-1]]
+gOp L xs = operate xs [[1,0],[0,-1]]  
